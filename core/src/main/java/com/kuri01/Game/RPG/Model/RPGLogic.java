3a786bb1de8cd159459b6730bec3d97ce1c014dc
Kuri01 <kurinchi@live.de>
2025-05-19 18:55:01 +0200

    RPG logic Test

---
+++ core/src/main/java/com/kuri01/Game/RPG/Model/RPGLogic.java
@@ -0,0 +1,40 @@
+package com.kuri01.Game.RPG.Model;
+
+import com.kuri01.Game.Card.Model.CardGameLogic;
+import com.kuri01.Game.Screen.GameScreen;
+
+public class RPGLogic {
+
+    private GameScreen gameScreen;
+    private CardGameLogic cardGameLogic;
+    Player player;
+    public Monster monster;
+
+    public RPGLogic(GameScreen gameScreen) {
+        this.gameScreen = gameScreen;
+        this.player = gameScreen.player;
+        this.createMonster();
+    }
+
+    public GameScreen getGameScreen() {
+        return gameScreen;
+    }
+
+    public CardGameLogic getCardGameLogic() {
+        return cardGameLogic;
+    }
+
+    public void createMonster() {
+        monster = new Monster("monster", 100, 5);
+
+    }
+
+    public void playerTakeDmg() {
+        player.takeDamage(monster.attack);
+    }
+
+    public void monsterTakeDmg() {
+        monster.takeDamage(player.attack);
+    }
+
+}
