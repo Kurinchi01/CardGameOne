8dbaa4a01f692b1946c81cf3f5817128dc642672
Kuri01 <kurinchi@live.de>
2025-06-24 13:16:09 +0200

    EquipmentItem.java und Item.java an Server angegleichen

---
+++ core/src/main/java/com/kuri01/Game/RPG/Model/ItemSystem/EquipmentItem.java
@@ -0,0 +1,25 @@
+package com.kuri01.Game.RPG.Model.ItemSystem;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EquipmentItem extends Item {
+    private EquipmentSlot equipmentSlot;
+    private Map<String, Integer> stats = new HashMap<>();
+
+    public EquipmentItem() {
+    }
+
+    public EquipmentSlot getEquipmentSlot() {
+        return equipmentSlot;
+    }
+
+    public void setEquipmentSlot(EquipmentSlot equipmentSlot) {
+        this.equipmentSlot = equipmentSlot;
+    }
+
+    public Map<String, Integer> getStats() {
+        return stats;
+    }
+
+}
--- core/src/main/java/com/kuri01/Game/RPG/Model/ItemSystem/Item.java
+++ core/src/main/java/com/kuri01/Game/RPG/Model/ItemSystem/Item.java
@@ -1,17 +1,17 @@
 package com.kuri01.Game.RPG.Model.ItemSystem;
-
-import static com.kuri01.Game.RPG.Model.Rarity.closest;
-
 import com.kuri01.Game.RPG.Model.Rarity;
 
 import java.util.Random;
 
 public class Item {
-    public float hp, atk, def, dex, crit, critdmg;
-    public final Rarity rarity;
-    Random r = new Random();
-    String fullName;
-    ItemType itemType;
+
+    private Long id;
+    public  Rarity rarity;
+    private String name;
+    private String description;
+
+
+
 //    String[] prefixE = {"Dunkle",
 //        "Glühende",
 //        "Verfluchte",
@@ -35,87 +35,9 @@ public class Item {
 //        "des Kriegers"};
 
     //ring
-    public Item(float hp, float atk, float def, float dex, float crit, float critdmg) {
-        this.hp = hp;
-        this.atk = atk;
-        this.def = def;
-        this.dex = dex;
-        this.crit = crit;
-        this.critdmg = critdmg;
-        this.rarity = closest(r.nextFloat()).get();
-        itemType = ItemType.Ri;
-        setName();
+    public Item() {
     }
 
-    //helmet&shoes
-    public Item(float hp, float def) {
-        this.hp = hp;
-        this.def = def;
-        this.rarity = closest(r.nextFloat()).get();
-        itemType = ItemType.He;
-        setName();
-    }
 
-    //gloves
-    public Item(float atk, float def, float dex, float crit) {
-        this.atk = atk;
-        this.def = def;
-        this.dex = dex;
-        this.crit = crit;
-        this.rarity = closest(r.nextFloat()).get();
-        itemType = ItemType.Ha;
-        setName();
-    }
 
-    //armor
-    public Item(float hp, float def, float dex) {
-        this.hp = hp;
-        this.def = def;
-        this.dex = dex;
-        this.rarity = closest(r.nextFloat()).get();
-        itemType = ItemType.Rue;
-        setName();
-    }
-
-    //chain
-    public Item(float hp, float critdmg, float dex, float def, float atk) {
-        this.hp = hp;
-        this.critdmg = critdmg;
-        this.dex = dex;
-        this.def = def;
-        this.atk = atk;
-        this.rarity = closest(r.nextFloat()).get();
-        itemType = ItemType.K;
-        setName();
-    }
-
-    public void setName() {
-        ItemType tmp = this.itemType;
-        switch (this.rarity) {
-            case common, uncommon -> {
-                if (tmp == ItemType.K || tmp == ItemType.Rue) {
-                    fullName = itemType + " " + sufix[r.nextInt(sufix.length)];
-                }
-                else {
-
-                }
-            }
-            case epic, rare -> {
-                fullName = prefix[r.nextInt(prefix.length)] + " " + itemType + " " + combiner[r.nextInt(combiner.length)] + " " + sufix[r.nextInt(sufix.length)];
-            }
-
-            case legendary -> {
-                int tmp1 = r.nextInt(prefix.length);
-                int tmp2 = r.nextInt(prefix.length);
-                fullName = prefix[r.nextInt(prefix.length)] + " " + itemType + " " + combiner[r.nextInt(combiner.length)] + " " + sufix[r.nextInt(sufix.length)];
-
-                while (tmp1 == tmp2) {
-                    fullName = prefix[r.nextInt(prefix.length)] + " " + itemType + " " + combiner[r.nextInt(combiner.length)] + " " + sufix[r.nextInt(sufix.length)];
-                    tmp1 = r.nextInt(prefix.length);
-                    tmp2 = r.nextInt(prefix.length);
-                }
-
-            }
-        }
-    }
 }
