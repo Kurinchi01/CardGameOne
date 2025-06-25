8e945a40ffcce79787f2031b557795cf78e75da6
Kuri01 <kurinchi@live.de>
2025-06-25 18:29:43 +0200

    Main implementiert und MainMenuScreen erstellt

---
+++ core/src/main/java/com/kuri01/Game/Screen/MainMenuScreen.java
@@ -0,0 +1,44 @@
+package com.kuri01.Game.Screen;
+
+import com.badlogic.gdx.Screen;
+import com.kuri01.Game.Main;
+
+public class MainMenuScreen implements Screen {
+    public MainMenuScreen(Main main) {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
