4fb58c79a35d0265fb8f46369cf7d866013c5984
Kuri01 <kurinchi@live.de>
2025-07-23 21:35:51 +0200

    neue DTOs für Ingamewährungsverwaltung hinzugefügt

--- core/src/main/java/com/kuri01/Game/RPG/Model/ItemSystem/DTO/PlayerDTO.java
+++ core/src/main/java/com/kuri01/Game/RPG/Model/ItemSystem/DTO/PlayerDTO.java
@@ -2,6 +2,13 @@ package com.kuri01.Game.RPG.Model.ItemSystem.DTO;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class PlayerDTO {
 
     // --- Basis-Charakterwerte ---
@@ -15,74 +22,10 @@ public class PlayerDTO {
     // --- Zusammengesetzte Objekte ---
     private EquipmentDTO equipmentDTO;
     private InventoryDTO inventory;
+    private PlayerWalletDTO playerWalletDTO;
 
-    // --- Leerer Konstruktor ---
 
-    public PlayerDTO() {}
 
-    // --- Getter & Setter ---
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public InventoryDTO getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(InventoryDTO inventory) {
-        this.inventory = inventory;
-    }
-
-    public EquipmentDTO getEquipmentDTO() {
-        return equipmentDTO;
-    }
-
-    public void setEquipmentDTO(EquipmentDTO equipmentDTO) {
-        this.equipmentDTO = equipmentDTO;
-    }
-
-    public float getAttack() {
-        return attack;
-    }
-
-    public void setAttack(float attack) {
-        this.attack = attack;
-    }
-
-    public float getMaxHp() {
-        return maxHp;
-    }
-
-    public void setMaxHp(float maxHp) {
-        this.maxHp = maxHp;
-    }
-
-    public int getExperiencePoints() {
-        return experiencePoints;
-    }
-
-    public void setExperiencePoints(int experiencePoints) {
-        this.experiencePoints = experiencePoints;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
---
+++ core/src/main/java/com/kuri01/Game/RPG/Model/ItemSystem/DTO/PlayerWalletDTO.java
@@ -0,0 +1,14 @@
+package com.kuri01.Game.RPG.Model.ItemSystem.DTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PlayerWalletDTO {
+    private long gold;
+    private long candy;
+
+}
